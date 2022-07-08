a007196e97f7a97cac27147cc25f187f4cab77fa
yonatani94 <yonatani94@gmil.com>
2022-07-08 13:51:37 +0300

    fixed head commit

--- src/main/java/start/logic/AdvancedItemsService.java
+++ src/main/java/start/logic/AdvancedItemsService.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package start.logic;
 
 import java.util.List;
@@ -15,19 +15,3 @@ public interface AdvancedItemsService extends ItemsService {
 	public List<TransactionBoundary> getWaitTransactionOnly(String userSpace, String userEmail,int size, int page);
 	
 }
-=======
-package start.logic;
-
-import java.util.List;
-
-import start.itemAPI.TransactionBoundary;
-
-
-
-public interface AdvancedItemsService extends ItemsService {
-	
-	
-	public List<TransactionBoundary> getAllItems(String userSpace, String userEmail,int size, int page);
-	public List<TransactionBoundary> getActiveItemsOnly(String userSpace, String userEmail,int size, int page);
-}
->>>>>>> c813bdfdde53cb9a25135a0dcf4dc84cf0ce9f7c
--- src/main/java/start/userAPI/UserController.java
+++ src/main/java/start/userAPI/UserController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package start.userAPI;
 
 
@@ -47,52 +46,3 @@ public class UserController {
 		this.userService.updateUser(userSpace, userEmail, user_boundry);
 	}
 }
-=======
-package start.userAPI;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import start.logic.UsersService;
-
-
-
-@RestController
-public class UserController {
-	
-	
-	private UsersService userService;
-	@Autowired
-	public void setUserService(UsersService userService) {
-		this.userService = userService;
-	}
-
-	
-	
-	@RequestMapping(path = "/blockchain/users", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public UserBoundary createUser(@RequestBody NewUserDetails userDeatalis) {
-		System.out.println(userDeatalis.toString());
-		UserBoundary user = this.userService.converNewtUserDeatailsToBoundary(userDeatalis);
-		return this.userService.createUser(user);
-	}
-
-	@RequestMapping(path = "/blockchain/users/login/{userSpace}/{userEmail}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public UserBoundary getUserSpace(@PathVariable("userSpace") String userSpace,
-			@PathVariable("userEmail") String userEmail) {
-		return this.userService.login(userSpace, userEmail);
-	}
-
-	@RequestMapping(path = "/blockchain/users/{userSpace}/{userEmail}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void updateUser(@PathVariable("userSpace") String userSpace, @PathVariable("userEmail") String userEmail,
-			@RequestBody UserBoundary user_boundry) {
-		this.userService.updateUser(userSpace, userEmail, user_boundry);
-	}
-}
->>>>>>> c813bdfdde53cb9a25135a0dcf4dc84cf0ce9f7c
